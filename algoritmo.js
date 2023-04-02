5bdc88e4d11c70ed2024642fdb89853525822330
Antu <122485403+antukung@users.noreply.github.com>
2023-04-02 20:59:02 -0300

    el comparador
    compara dos numeros entre el 1 y el dos

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+let n1 =prompt ("dame un numero");
+let n2 =prompt ("dame el segundo numero");
+    if ( n1 === n2 ) {
+        document.write ("<h1>son iguales los numeros</h1>");
+    } else {
+        document.write ("<h2>son distintos los dos numeros</h2>");
+    }
+
