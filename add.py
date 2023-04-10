ff6d9e3c3968e3bd03ad5823437ae18072614543
Antu <122485403+antukung@users.noreply.github.com>
2023-04-10 15:11:20 -0300

    python
    lo mismo hecho en python

---
+++ add.py
@@ -0,0 +1,8 @@
+print ("dame un numero")
+n1 = int(input ())
+print ("dame otro numero")
+n2 = int(input ())
+if n1 == n2 :
+        print ("los dos numeros son iguales")
+else :
+    print ("son distintos")
